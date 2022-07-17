9944aa21be7fa64de70e847f4d33d92a3fe57784
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-07-16 20:18:52 -0500

    add open telemetry to mongo and gin

--- database/init.go
+++ database/init.go
@@ -5,22 +5,23 @@ import (
 	"time"
 
 	"github.com/alehechka/mongodb-playground/constants"
+	"github.com/alehechka/mongodb-playground/opentel"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
 )
 
 var client *mongo.Client
 var database *mongo.Database
 var podcastCollection *mongo.Collection
 var episodeCollection *mongo.Collection
-var timeoutContext context.Context
 
 // InitializeMongoDB initializes global MongoDB client
 func InitializeMongoDB() (disconnect func() error, err error) {
 	timeoutContext, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
-	client, err = mongo.Connect(timeoutContext, options.Client().ApplyURI(constants.MongoDBConnectionString))
+	client, err = mongo.Connect(timeoutContext, options.Client().ApplyURI(constants.MongoDBConnectionString).SetMonitor(otelmongo.NewMonitor(otelmongo.WithTracerProvider(opentel.TracerProvider))))
 	if err != nil {
 		cancel()
 		return nil, err
--- rest/handlers.go
+++ rest/handlers.go
@@ -2,12 +2,16 @@ package rest
 
 import (
 	"github.com/alehechka/go-utils/ginshared"
+	"github.com/alehechka/mongodb-playground/opentel"
+	"github.com/alehechka/mongodb-playground/types"
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
 func SetupRouter() *gin.Engine {
 	engine := gin.Default()
 	engine.Use(ginshared.CorsConfigMiddleware)
+	engine.Use(otelgin.Middleware("mongodb-playground-api", otelgin.WithTracerProvider(opentel.TracerProvider)))
 
 	RegisterHandlers(engine)
 
@@ -21,18 +25,18 @@ func RegisterHandlers(engine *gin.Engine) {
 	{
 		podcasts := router.Group("/podcasts")
 		podcasts.GET("", getPodcasts)
-		podcasts.GET("/:podcastID", getPodcast)
+		podcasts.GET("/:"+types.PodcastID, getPodcast)
 		podcasts.POST("", createPodcast)
-		podcasts.PUT("/:podcastID", replacePodcast)
-		podcasts.DELETE("/:podcastID", deletePodcast)
+		podcasts.PUT("/:"+types.PodcastID, replacePodcast)
+		podcasts.DELETE("/:"+types.PodcastID, deletePodcast)
 
 		{
-			episodes := podcasts.Group("/:podcastID/episodes")
+			episodes := podcasts.Group("/:" + types.PodcastID + "/episodes")
 			episodes.GET("", getPodcastEpisodes)
-			episodes.GET("/:episodeID", getPodcastEpisode)
+			episodes.GET("/:"+types.EpisodeID, getPodcastEpisode)
 			episodes.POST("", createPodcastEpisode)
-			episodes.PUT("/:episodeID", replacePodcastEpisode)
-			episodes.DELETE("/:episodeID", deletePodcastEpisode)
+			episodes.PUT("/:"+types.EpisodeID, replacePodcastEpisode)
+			episodes.DELETE("/:"+types.EpisodeID, deletePodcastEpisode)
 		}
 	}
 }
